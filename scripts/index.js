e13b3ae01be0c0761d6d290fd05870c00f7fb3f7
Lisa Dean <lisa@lisadean.net>
2018-05-25 10:36:34 -0400

    Changed image alt to title. Changed header font color. Refactored javascript.

--- scripts/index.js
+++ scripts/index.js
@@ -13,8 +13,6 @@ const NUM_OF_COMICS = 10;
 // Those files contain, in a plaintext and easily-parsed format: comic titles, 
 // URLs, post dates, transcripts (when available), and other metadata.
 
-var latestComic = PROXY + URL + REQ;
-
 function getRequestStrings(num, latestNum) {
   var results = [];
   for (var i=latestNum; i >= latestNum - 10; i--) {
@@ -24,58 +22,49 @@ function getRequestStrings(num, latestNum) {
 }
 
 function addComic(data) {
-  
+  var $wrapperDiv = $('[data-wrapper]');
+  var $comicDiv = $('<div>');
   var $image = $('<img>');
+  var $imageCaptionDiv = $('<div>');
+  var $titleDiv = $('<div>');
+  var $issueDiv = $('<div>');
+  var $publishDateDiv = $('<div>');
+  
+  $comicDiv.addClass('comic');
+  $image.addClass('comic-image');
+  $imageCaptionDiv.addClass('caption');
+  $titleDiv.addClass('title');
+  $issueDiv.addClass('issue');
+  $publishDateDiv.addClass('publish-date');
+  
   $image.attr('src', data.img);
   $image.attr('title', data.alt);
-  $image.addClass('comic-image');
-
-  var $titleDiv = $('<div>');
   $titleDiv.text(data.title);
-  $titleDiv.addClass('title');
-
-  // var $detailsDiv = $('<div>');
-  // $detailsDiv.text(`#${data.num}\n${data.month}/${data.day}/${data.year}`);
-  // $detailsDiv.addClass('details');
-
-  var $issueDiv = $('<div>');
   $issueDiv.text(`#${data.num}`);
-  $issueDiv.addClass('issue');
-
-  var $publishDateDiv = $('<div>');
   $publishDateDiv.text(`${data.month}/${data.day}/${data.year}`);
-  $publishDateDiv.addClass('publish-date');
 
-  var $imageCaptionDiv = $('<div>');
-  $imageCaptionDiv.addClass('caption');
   $imageCaptionDiv.append($titleDiv);
   $imageCaptionDiv.append($issueDiv);
   $imageCaptionDiv.append($publishDateDiv);
-  // $imageCaptionDiv.append($detailsDiv);
-  
-
-  var $comicDiv = $('<div>');
-  $comicDiv.addClass('comic');
   $comicDiv.append($image);
   $comicDiv.append($imageCaptionDiv);
-
-  $('[data-wrapper]').append($comicDiv);
-}
-
-function doEeeeetAll() {
-  $.get(latestComic, latestComicData => {
-    var requestStrings = getRequestStrings(NUM_OF_COMICS, latestComicData.num);
-    requestStrings.forEach(string => {
-      $.get(string, singleComicData => {
-        addComic(singleComicData);
-      });
-    });
-  });
+  $wrapperDiv.append($comicDiv);
 }
 
 function main() {
   $('.header').text(`Last ${NUM_OF_COMICS} xkcd Comics`);
-  doEeeeetAll();
+
+  var latestComicURL = PROXY + URL + REQ;
+  $.get(latestComicURL, latestComicData => {
+
+    var requestStrings = getRequestStrings(NUM_OF_COMICS, latestComicData.num);
+    requestStrings.forEach(URLString => {
+      $.get(URLString, singleComicData => {
+        
+        addComic(singleComicData);
+      });
+    });
+  });
 }
 
 main();
